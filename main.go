bcbccf475a775d0b35928fed2058c58d76ec4fc9
Thomas Perez <thomas@scullwm.com>
2017-06-23 13:29:53 +0200

    Use yaml configuration

--- main.go
+++ main.go
@@ -9,7 +9,19 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/robfig/cron"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
+
+type DatabaseConfig struct {
+	Connection struct {
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		Name     string `yaml:"name"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+	}
+}
 
 func main() {
 	heartbeatIntervalString := GetEnvVar("HEARTBEAT_INTERVAL", "5")
@@ -39,11 +51,16 @@ func JobPrint(jobName string, message string) {
 }
 
 func GetDatabaseConnection() *sql.DB {
-	dbHost := GetEnvVar("DATABASE_HOST", "127.0.0.1")
-	dbPort := GetEnvVar("DATABASE_PORT", "3306")
-	dbName := GetEnvVar("DATABASE_NAME", "enmarche")
-	dbUser := GetEnvVar("DATABASE_USER", "root")
-	dbPass := GetEnvVar("DATABASE_PASSWORD", "root")
+	config, err := GetDatabaseConfiguration()
+	if err != nil {
+		panic(err)
+	}
+
+	dbHost := GetEnvVar("DATABASE_HOST", config.Connection.Host)
+	dbPort := GetEnvVar("DATABASE_PORT", config.Connection.Port)
+	dbName := GetEnvVar("DATABASE_NAME", config.Connection.Name)
+	dbUser := GetEnvVar("DATABASE_USER", config.Connection.User)
+	dbPass := GetEnvVar("DATABASE_PASSWORD", config.Connection.Password)
 	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName
 
 	db, err := sql.Open("mysql", dsn)
@@ -56,6 +73,21 @@ func GetDatabaseConnection() *sql.DB {
 	}
 
 	return db
+}
+
+func GetDatabaseConfiguration() (*DatabaseConfig, error) {
+	config := &DatabaseConfig{}
+
+	bytes, err := ioutil.ReadFile("./config.yml")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(bytes, config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
 }
 
 func GetEnvVar(key, fallback string) string {
